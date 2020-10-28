bd8330fcce390b7d3cd32e0447a9792aac17a669
Advitya Gemawat <agemawat@ucsd.edu>
2020-10-28 12:08:57 -0700

    Run arguments

--- run.py
+++ run.py
@@ -1,5 +1,30 @@
-def main():
-    print("Hello World!")
+import pprint
+import pdb
+import arrow
+import json
+import sys
+import os
+import time
+import logging
+import csv
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator, AutoLocator, FixedLocator
+# from building_depot import DataService, BDError
+from datetime import timedelta
+from cycler import cycler
+from etl import load_uuid_data
+
+sys.path.append('../src')
 
 if __name__ == "__main__":
-    main()
+    action = sys.argv[1]
+    if action == 'env-setup':
+        os.system('pip install -r requirements.txt')
+        print('Requirements installed!')
+    elif action == 'data':
+        load_uuid_data()
+        print("Sensor points data loaded in 'sensor_uuids.json'!")
+    else:
+        print('Please specify a valid argument!')
