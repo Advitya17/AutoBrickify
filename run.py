a2e24d00aa830c491b68637763dd99539361b01f
Advitya Gemawat <agemawat@ucsd.edu>
2020-11-14 20:55:46 -0800

    Re-factored and updated actions

--- run.py
+++ run.py
@@ -2,16 +2,30 @@ import sys
 sys.path.append('./src')
 import os
 
-if __name__ == "__main__":
-    action = sys.argv[1]
-    if action == 'env-setup':
+def execute(action_arg):
+    run_all = action_arg == 'all'
+    
+    if action_arg == 'clean' or run_all:
+        clean()
+    
+    if action_arg == 'env-setup' or run_all:
+        from etl import load_bd_api
+        load_bd_api()
         os.system('pip install -r requirements.txt')
-        print('Requirements installed!')
-    elif action == 'data':
+        print('API loaded and requirements installed!')
+    
+    if action_arg == 'data' or run_all:
         from etl import load_uuid_data, load_co2_and_humidity_data
         load_uuid_data()
         print("Sensor points data loaded in 'sensor_uuids.json'!")
         load_co2_and_humidity_data()
         print("CO2 and Humidity data loaded in the 'data' directory")
-    else:
-        print('Please specify a valid argument!')
+    
+    if action_arg == 'plot' or run_all:
+        print('Will be fully implemented for checkpoint-3. Stay tuned!')
+    
+    # print('Please specify a valid argument!')
+
+if __name__ == "__main__":
+    action = sys.argv[1]
+    execute(action)
