856183118e67c8e7d56b55c08671b9a1abb2af68
Advitya Gemawat <agemawat@ucsd.edu>
2020-11-14 21:29:14 -0800

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ def execute(action_arg):
     run_all = action_arg == 'all'
     
     if action_arg == 'clean' or run_all:
-        clean()
+        from etl import clean_extra_contents
+        clean_extra_contents()
     
     if action_arg == 'env-setup' or run_all:
         from etl import extract_bd_api
