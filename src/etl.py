4b8b925e4c8ce0b8047779668276fd70a290047f
advitya gemawat <agemawat@ucsd.edu>
2020-11-24 22:27:41 -0800

    bug fixes

--- src/etl.py
+++ src/etl.py
@@ -14,34 +14,39 @@ import csv
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator, AutoLocator, FixedLocator
-from building_depot import DataService, BDError
 from datetime import timedelta
 from cycler import cycler
 
 # sys.path.append('../config')
 
-# load config
-config = json.load(open('config/data-params.json')) # '../data-params.json'
-cse_dataservice_url = config["cse_dataservice_url"]
-bd_username = config["bd_username"]
-bd_api_key = config["bd_api_key"]
-remote_sensors = config["remote_sensors"]
+etl_params = json.load(open('config/etl-params.json'))
+source_repo = etl_params['source_repo']
+repo_name = etl_params['repo_name']
+api_fp = etl_params['api_fp']
+data_fp = etl_params['data_fp']
 
-etl_config = json.load(open('config/etl-params.json')) # '../data-params.json'
-source_repo = etl_config["source_repo"]
-repo_name = etl_config["repo_name"]
-api_fp = etl_config["api_fp"]
-data_fp = etl_config["data_fp"]
-
-#Connect with BuildingDepot
-ds = DataService(cse_dataservice_url, bd_api_key, bd_username)
-
+# Connect with BuildingDepot
 def extract_bd_api():
     os.system('git clone ' + source_repo)
     os.system('mv ' + api_fp + ' ../src/building_depot')
     os.system('rm -rf ./' + repo_name)
 
 def load_uuid_data():
+    from building_depot import DataService, BDError
+
+    # load config
+    config = json.load(open('config/data-params.json')) # '../data-params.json'
+    cse_dataservice_url = config["cse_dataservice_url"]
+    bd_username = config["bd_username"]
+    bd_api_key = config["bd_api_key"]
+    remote_sensors = config["remote_sensors"]
+
+    etl_config = json.load(open('config/etl-params.json')) # '../data-params.json'
+    source_repo = etl_config["source_repo"]
+    repo_name = etl_config["repo_name"]
+    api_fp = etl_config["api_fp"]
+    data_fp = etl_config["data_fp"]
+
     data = {}
     for room in list(config["target_rooms_setpoint_values"].keys())+list(config["uncontrolled_rooms"].keys()):
         query = {
