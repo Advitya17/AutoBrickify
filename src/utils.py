fc4e09a3f6ac661b2a9b2f3800a5e0ceee95c184
agemawat <agemawat@ucsd.edu>
2020-11-25 16:51:50 -0800

    Testing updates
    Former-commit-id: b7121670da00275853003554cef76e05fe859505

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
 import os, sys
 
 def clean_extra_contents():
-    os.system('rm -rf ./building_depot ../data ../plots /config/sensor_uuids.json')
+    os.system('rm -rf src/building_depot data plot logs ./config/sensor_uuids.json')
