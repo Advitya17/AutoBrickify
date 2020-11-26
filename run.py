4b6172b045498c66e3149c82317a9b65e0556368
Devanshu Desai <dbdesai@ucsd.edu>
2020-11-25 18:46:25 -0800

    fixed imports

--- run.py
+++ run.py
@@ -6,22 +6,22 @@ def execute(action_arg):
     run_all = action_arg == 'all'
 
     if action_arg == 'test':
-        from execute import run_test
+        from src.execute import run_test
         run_test()
 
-    if action_arg == 'clean' or run_all:
-        from etl import clean_extra_contents
-        clean_extra_contents()
+    # if action_arg == 'clean' or run_all:
+    #     from src.etl import clean_extra_contents
+    #     clean_extra_contents()
 
     if action_arg == 'env-setup' or run_all:
         os.system('pip install -r requirements.txt')
         print('Requirements installed!')
 
     if action_arg == 'data' or run_all:
-        from etl import extract_bd_api
+        from src.etl import extract_bd_api
         extract_bd_api()
         print('Building Depot API loaded!')
-        from etl import load_uuid_data, load_co2_and_humidity_data
+        from src.etl import load_uuid_data, load_co2_and_humidity_data
         load_uuid_data()
         print("Sensor points data loaded in 'sensor_uuids.json'!")
         load_co2_and_humidity_data()
