ae3dd2dcb54dc8023d6881732e701ed8f64a1152
Advitya Gemawat <agemawat@ucsd.edu>
2020-12-09 21:20:06 -0800

    run script re-factor
    Former-commit-id: 98158d2566f26df673f080b9dfcbd84a8d1c7546

--- run.py
+++ run.py
@@ -3,40 +3,57 @@ sys.path.append('./src')
 import os
 
 def execute(action_arg):
-    run_all = action_arg == 'all'
+    # TODO: remove
+    # run_all = action_arg == 'all'
+    # or run_all (stripped condition)
     
-    if action_arg == 'clean' or run_all:
+    if action_arg == 'clean':
         from utils import clean_extra_contents
         clean_extra_contents()
     
-    if action_arg == 'env-setup' or run_all:
-        os.system('pip install -r requirements.txt --quiet')
-        print('Requirements installed!')
-        from etl import extract_bd_api
-        extract_bd_api()
+    elif action_arg == 'env-setup':
+        # TODO: check if need to install requirements
+        # remove after documentation update for docker
+        # os.system('pip install -r requirements.txt --quiet')
+        # print('Requirements installed!')
+        
+        # needed system call to set right permission 
+        # and load API as per feedback
+        os.system('chmod +x load_api.sh')
+        os.system('sh load_api.sh')
         print('Building Depot API loaded!')
+        
+        # TODO: remove!
+        # from etl import extract_bd_api
+        # extract_bd_api()
+        # print('Building Depot API loaded!')
     
-    if action_arg == 'data' or run_all:
+    elif action_arg == 'data':
         from etl import load_uuid_data, load_co2_and_humidity_data
+        
         load_uuid_data()
         print("Sensor points data loaded in 'sensor_uuids.json'!")
-        # Uncomment for checkpoint-3
-        # load_co2_and_humidity_data()
-        # print("CO2 and Humidity data loaded in the 'data' directory!")
+        
+        load_co2_and_humidity_data()
+        print("CO2 and Humidity data loaded in the 'data' directory!")
     
-    if action_arg == 'test':
+    elif action_arg == 'test':
         # env-setup?
         execute('clean')
+        execute('env-setup')
         execute('data')
         # from execute import run_test
         # run_test()
         # print('Test plot saved in a pdf file inside ./plot/2020-07-30!')
         execute('plot')
     
-    if action_arg == 'plot' or run_all:
-        print('Will be fully executed in checkpoint-3. Stay tuned!')
+    elif action_arg == 'plot':
+        from execute import plot
+        plot()
+        print('All plots saved in ./plot!')
     
-    # print('Please specify a valid argument!')
+    else:
+        print('Please specify a valid argument!')
 
 if __name__ == "__main__":
     action = sys.argv[1]
