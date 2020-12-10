9ff26326aa313f2b082a453fac9378e49006b721
Advitya Gemawat <agemawat@ucsd.edu>
2020-12-09 20:41:32 -0800

    Update run.py
    Former-commit-id: b3a19b0342cdaeb26bf2dda02d885bb710781f3f

--- run.py
+++ run.py
@@ -12,11 +12,11 @@ def execute(action_arg):
     if action_arg == 'env-setup' or run_all:
         os.system('pip install -r requirements.txt --quiet')
         print('Requirements installed!')
-    
-    if action_arg == 'data' or run_all:
         from etl import extract_bd_api
         extract_bd_api()
         print('Building Depot API loaded!')
+    
+    if action_arg == 'data' or run_all:
         from etl import load_uuid_data, load_co2_and_humidity_data
         load_uuid_data()
         print("Sensor points data loaded in 'sensor_uuids.json'!")
