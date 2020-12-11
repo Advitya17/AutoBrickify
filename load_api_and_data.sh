8ca4e939d5a8083dcc150f0222024e45cd39885b
Advitya Gemawat <agemawat@ucsd.edu>
2020-12-10 17:40:10 -0800

    Update and rename load_api.sh to load_api_and_data.sh
    Former-commit-id: 2e0dd29f66f8cb3edde29bc3b5d393076fe74bac

--- load_api_and_data.sh
+++ load_api_and_data.sh
@@ -1,4 +1,10 @@
-# Loads BD API
+
 git clone https://gitlab.com/dzhong1989/hvac-safety-control.git --quiet
+
+# Loads BD API
 mv ./hvac-safety-control/building_depot src/building_depot
+
+# Loads data
+mv ./hvac-safety-control/CO2_data data
+
 rm -rf ./hvac-safety-control
