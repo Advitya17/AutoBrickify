036ea4e56573b4a248d5f6cf6de4347cd5ef12ad
Devanshu Desai <dbdesai@ucsd.edu>
2021-01-13 01:00:38 -0800

    added reconcile API
    Former-commit-id: d2cbcbcdb209b31e59e8e38a0eb8ee93dd96f57c

--- run.py
+++ run.py
@@ -1,10 +1,63 @@
 import pandas as pd
 import numpy as np
 import os
+from abbrmap import abbrmap as tagmap
+import json
+import brickschema
+import re
 import sys
 sys.path.append('./src')
 
-def automatic_OR(fp='table_743_v1.csv'):
+metadata = {
+    "name": "Brick Reconciliation Service",
+    "defaultTypes": [
+        {"id": "EquipmentClass", "name": "EquipmentClass"},
+        {"id": "PointClass", "name": "PointClass"}
+    ]
+}
+
+inf = brickschema.inference.TagInferenceSession(approximate=True)
+
+
+def flatten(lol):
+    """flatten a list of lists"""
+    return [x for sl in lol for x in sl]
+
+
+def resolve(q):
+    """
+    q has fields:
+    - query: string of the label that needs to be converted to a Brick type
+    - type: optional list of 'types' (e.g. "PointClass" above)
+    - limit: optional limit on # of returned candidates (default to 10)
+    - properties: optional map of property idents to values
+    - type_strict: [any, all, should] for strictness on the types returned
+    """
+    limit = int(q.get('limit', 10))
+    # break query up into potential tags
+    tags = map(str.lower, re.split(r'[.:\-_ ]', q.get('query', '')))
+    tags = list(tags)
+    brick_tags = flatten([tagmap.get(tag.lower(), [tag]) for tag in tags])
+
+    if q.get('type') == 'PointClass':
+        brick_tags += ['Point']
+    elif q.get('type') == 'EquipmentClass':
+        brick_tags += ['Equipment']
+
+    res = []
+    most_likely, leftover = inf.most_likely_tagsets(brick_tags, limit)
+    for ml in most_likely:
+        res.append({
+            'id': q['query'],
+            'name': ml,
+            'score': (len(brick_tags) - len(leftover)) / len(brick_tags),
+            'match': len(leftover) == 0,
+            'type': [{"id": "PointClass", "name": "PointClass"}],
+        })
+    print('returning', res)
+    return res
+
+def automatic_OR(fp='data/table_743_v1.csv'):
     """Automates work of Open Refine"""
     df = pd.read_csv(fp)
 
