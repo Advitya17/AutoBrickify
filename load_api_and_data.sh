65f46171f1b1f9db6f4a1cbab52d8f720f19a0a9
Advitya Gemawat <agemawat@ucsd.edu>
2021-01-19 22:25:54 -0800

    Update load_api_and_data.sh
    Former-commit-id: b5235cf30352bb7c340c5616d22cdb1405adccec

--- load_api_and_data.sh
+++ load_api_and_data.sh
@@ -1,10 +1,3 @@
 
+# TODO: reconciliation API ?!
 git clone https://gitlab.com/dzhong1989/hvac-safety-control.git --quiet
-
-# Loads BD API
-mv ./hvac-safety-control/building_depot src/building_depot
-
-# Loads data
-mv ./hvac-safety-control/CO2_data data
-
-rm -rf ./hvac-safety-control
