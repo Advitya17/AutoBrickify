54ab16c8520711b6940226452c224f113ff332e6
Advitya Gemawat <agemawat@ucsd.edu>
2021-01-26 23:00:46 -0800

    Update load_api_and_data.sh
    Former-commit-id: 5b51d90a67bddde44c121b3f9ec4810e81a357dd

--- load_api_and_data.sh
+++ load_api_and_data.sh
@@ -1,5 +1,6 @@
 # reconciliation API ?!
 
 # brick builder tool
-git clone https://github.com/gtfierro/brick-builder --quiet
+# TODO : https://github.com/gtfierro/brick-builder
+git clone https://github.com/devanshuDesai/brick-builder --quiet
 mv brick-builder ../brick-builder
