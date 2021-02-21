8cce1deb9d3113ea26f13aa99ae7e2ef99c57090
Advitya Gemawat <agemawat@ucsd.edu>
2021-02-20 16:30:16 -0800

    Update and rename load_api_and_data.sh to dependencies.sh
    Former-commit-id: 03816e4c56842d7f10e018587bcf7e05cfacf830

--- dependencies.sh
+++ dependencies.sh
@@ -2,5 +2,6 @@
 
 # brick builder tool
 # TODO : https://github.com/gtfierro/brick-builder
+pip install -r ../brick-builder/requirements.txt --quiet
 git clone https://github.com/devanshuDesai/brick-builder --quiet
 mv brick-builder ../brick-builder
