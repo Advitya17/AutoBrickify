97e851038923179652fa8dc6108b7c1464329774
Devanshu Desai <dbdesai@ucsd.edu>
2021-03-02 00:20:45 -0800

    added updated abbrmap to setup process
    Former-commit-id: 292c353625fff075d4bbf9fa6738e40b6cc58528

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,9 @@
-# reconciliation API ?!
-
 # brick builder tool
 # TODO : https://github.com/gtfierro/brick-builder
 pip install -r ../brick-builder/requirements.txt --quiet
 git clone https://github.com/devanshuDesai/brick-builder --quiet
 mv brick-builder ../brick-builder
+
+# reconciliation API
+git clone https://github.com/Advitya17/reconciliation-api ../reconciliation-api --quiet
+cp ../reconciliation-api/abbrmap.py .
