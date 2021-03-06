d2d3e158601b772f37d4fca5fc807eb6fcd5d34a
Advitya Gemawat <agemawat@ucsd.edu>
2021-03-06 12:18:58 -0800

    Update dependencies.sh
    Former-commit-id: e14f5ae5f9b50ee817bd560961f2c84a7dec20dc

--- dependencies.sh
+++ dependencies.sh
@@ -5,5 +5,6 @@ git clone https://github.com/devanshuDesai/brick-builder --quiet
 mv brick-builder ../brick-builder
 
 # reconciliation API
+# TODO: https://github.com/BrickSchema/reconciliation-api
 git clone https://github.com/Advitya17/reconciliation-api ../reconciliation-api --quiet
 cp ../reconciliation-api/abbrmap.py .
