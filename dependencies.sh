00857f8e9339aa9776056f2c3c8b7aa453350603
Advitya Gemawat <agemawat@ucsd.edu>
2021-03-07 23:29:05 -0800

    Update dependencies.sh
    Former-commit-id: 0d814ef15a27ee4e572121b371b5215cf7ec3a59

--- dependencies.sh
+++ dependencies.sh
@@ -1,8 +1,8 @@
 # brick builder tool
 # TODO : https://github.com/gtfierro/brick-builder
-pip install -r ../brick-builder/requirements.txt --quiet
 git clone https://github.com/devanshuDesai/brick-builder --quiet
 mv brick-builder ../brick-builder
+pip install -r ../brick-builder/requirements.txt --quiet
 
 # reconciliation API
 # TODO: https://github.com/BrickSchema/reconciliation-api
